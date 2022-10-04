eaa3b5dcfa4d58cfcdca25fabfeac51bcfe3749d
Josh Bressers <josh@bress.net>
2022-10-04 13:17:14 -0500

    Use a rest API

--- server/server.js
+++ server/server.js
@@ -1,45 +1,19 @@
-// Get net module 
-const net = require('net');
+var NodeRestServer = require('node-rest-server');
 
-//Configuration ===================================
-const port = 8888;
-//=================================================
-
-
-//Create an instance of the server and attach the client handling callback
-const server = net.createServer(onClientConnection);
-
-//Start listening on given port and host.
-server.listen(port,function(){
-   console.log(`Server started on port ${port}`); 
-});
-
-
-//the client handling callback
-function onClientConnection(sock){
-    //Log when a client connnects.
-    console.log(`${sock.remoteAddress}:${sock.remotePort} Connected`);
-    
-	//Handle the client data.
-    sock.on('data',function(data){
-        //Log data received from the client
-        console.log(`>> data received : ${data} `);
-		
-		//prepare and send a response to the client 
-		let serverResp = "Hello from the server"
-		sock.write(serverResp);
-		
-		//close the connection 
-		sock.end()        
-	});
-    
-	//Handle when client connection is closed
-    sock.on('close',function(){
-        console.log(`${sock.remoteAddress}:${sock.remotePort} Connection closed`);
-    });
-    
-	//Handle Client connection error.
-    sock.on('error',function(error){
-        console.error(`${sock.remoteAddress}:${sock.remotePort} Connection Error ${error}`);
-    });
+const routeConfig = {
+    '/': {
+        method: 'GET',
+        status: 200,
+        controller: () => 'Data',
+    },
 };
+
+const serverConfig = {
+  port: 8888,
+  logger: {
+      enable: true,
+      debug: false,
+  }
+}
+
+NodeRestServer(routeConfig, serverConfig);
